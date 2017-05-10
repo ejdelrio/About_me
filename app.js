365334239c8b98bee064fd149df8673672b84dae
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-05-10 16:39:54 -0700

    final edits to complete app.js

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
 'use strict';
-// My do whil loop is included at the bottom in the stretch section
+// My do while loop is included at the bottom in the stretch section
+var userName = prompt('What\'s your name????');
+var languages = ['english', 'spanish', 'thai'];
 var answers = ['y','yes','n', 'no' ];
 var output = [];
 var questions = [
   'play an instrument',
   'speak a foreign language',
-  'know how to dance',
-  'have any children',
+  'like to gorge',
+  'drive a rust bucket',
   'like scandanavian death metel',
-  'like the backstreet boys',
   'Do you believe in rock and roll?',
   'Can music save your mortal soul?',
   'And can you teach me how to dance real slow?'
 ];
 
-for (var i = 0; i < 6; i++) {
+
+for (var i = 0; i < 5; i++) {
   while (true) {
-      var userInput = prompt('Do you ' + questions[i] + '?\n Please enter y or n.').toLowerCase();
+      var userInput = prompt('Do I ' + questions[i] + '?\n Please enter y or n.').toLowerCase();
       console.log('user response: ', userInput)
       if (answers.includes(userInput)) {
-          ouput.push(userInput);
+          output.push(userInput);
           break;
       } else {
           alert("That is an not a valid response. Try again.");
@@ -27,9 +29,17 @@ for (var i = 0; i < 6; i++) {
     }
 }
 
-
-
-//These are the stretch goals from github
+for (var i =0; i < 6; i++) {
+  var languageGuess = prompt('Can you guess what languages I speak?').toLowerCase();
+  if (languages.includes(languageGuess)) {
+    alert('Correct! I You got lucky.');
+    output.push('y')
+    break
+  } else {
+    alert('Nope!')
+  }
+}
+alert('the correct answers were: ' + languages.join(", "))
 
 var randNum = Math.floor(Math.random() * 20 + 1);
 var count = 0
@@ -38,12 +48,28 @@ var count = 0
   console.log('users guess: ', numGuess)
     if (numGuess === randNum) {
       alert('Congratulations!!! You guessed correctly');
+      output.push('y')
       break;
     } else {
-      alert('Nope. You have ' + (5 - (count + 1)) + ' tries left');
+      alert('Nope. You have ' + (4 - (count + 1)) + ' tries left');
     }
-    if (count === 4) {
+    if (count === 3) {
       alert('You\'re all out of turns. You lose!! :D')
     }
     count ++
-} while (count != 5);
+} while (count != 4);
+
+var correct = 0;
+for (var i =0, l = output.length; i < l; i++) {
+  if (['y', 'yes'].includes(output[i])) {
+    correct += 1;
+  }
+}
+
+alert('Congratulations ' + userName + '!!! You got ' + correct + ' question correct');
+
+
+alert('last questions. I promise!!')
+for (var i = 5, l = questions.length; i < l; i++){
+  prompt(questions[i] + '?');
+}
